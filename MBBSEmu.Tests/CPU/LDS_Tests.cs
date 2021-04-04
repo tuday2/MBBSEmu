25474c01665d55fb273bd722590b9fac83d47b38
Jonathan Sider <jsider@gmail.com>
2021-04-04 06:15:59 -0500

    Create LDS Tests

---
+++ MBBSEmu.Tests/CPU/LDS_Tests.cs
@@ -0,0 +1,36 @@
+﻿using Iced.Intel;
+using Xunit;
+using static Iced.Intel.AssemblerRegisters;
+
+namespace MBBSEmu.Tests.CPU
+{
+    public class LDS_Tests : CpuTestBase
+    {
+        [Theory]
+        [InlineData(1, 4)]
+        [InlineData(ushort.MaxValue, 6)]
+        [InlineData(ushort.MinValue, 8)]
+        public void LDS_R16_M16(ushort memValue, ushort memLocation)
+        {
+            Reset();
+
+            mbbsEmuProtectedModeMemoryCore.AddSegment(2);
+            mbbsEmuMemoryCore.SetWord(2, memLocation, memValue);
+
+            var instructions = new Assembler(16);
+            instructions.lds(si, __word_ptr[memLocation]);
+            CreateCodeSegment(instructions);
+
+            //Process Instruction
+            mbbsEmuCpuCore.Tick();
+
+            //Verify Results
+            Assert.Equal(memLocation, mbbsEmuCpuRegisters.SI);
+            Assert.Equal(memValue, mbbsEmuMemoryCore.GetWord(2, memLocation));
+            Assert.False(mbbsEmuCpuRegisters.OverflowFlag);
+            Assert.False(mbbsEmuCpuRegisters.CarryFlag);
+            Assert.False(mbbsEmuCpuRegisters.ZeroFlag);
+            Assert.False(mbbsEmuCpuRegisters.SignFlag);
+        }
+    }
+}

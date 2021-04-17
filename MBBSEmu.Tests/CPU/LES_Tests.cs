912fe0a863f15fe25407900efa2d3759f2285e79
Jonathan Sider <jsider@gmail.com>
2021-04-17 16:37:51 -0500

    Add LES Tetss

--- MBBSEmu.Tests/CPU/LES_Tests.cs
+++ MBBSEmu.Tests/CPU/LES_Tests.cs
@@ -4,13 +4,13 @@ using static Iced.Intel.AssemblerRegisters;
 
 namespace MBBSEmu.Tests.CPU
 {
-    public class LDS_Tests : CpuTestBase
+    public class LES_Tests : CpuTestBase
     {
         [Theory]
         [InlineData(1, 4)]
         [InlineData(ushort.MaxValue, 6)]
         [InlineData(ushort.MinValue, 8)]
-        public void LDS_R16_M16(ushort memValue, ushort memLocation)
+        public void LES_R16_M16(ushort memValue, ushort memLocation)
         {
             Reset();
 
@@ -19,7 +19,7 @@ namespace MBBSEmu.Tests.CPU
             mbbsEmuMemoryCore.SetWord(2, memLocation, memValue);
 
             var instructions = new Assembler(16);
-            instructions.lds(si, __word_ptr[memLocation]);
+            instructions.les(di, __word_ptr[memLocation]);
             CreateCodeSegment(instructions);
 
             //Process Instruction
@@ -37,7 +37,7 @@ namespace MBBSEmu.Tests.CPU
         [InlineData(1, 4)]
         [InlineData(uint.MaxValue, 6)]
         [InlineData(uint.MinValue, 8)]
-        public void LDS_R32_M16(uint memValue, ushort memLocation)
+        public void LES_R32_M16(uint memValue, ushort memLocation)
         {
             Reset();
 
@@ -46,7 +46,7 @@ namespace MBBSEmu.Tests.CPU
             mbbsEmuMemoryCore.SetDWord(2, memLocation, memValue);
 
             var instructions = new Assembler(16);
-            instructions.lds(si, __dword_ptr[memLocation]);
+            instructions.les(di, __dword_ptr[memLocation]);
             CreateCodeSegment(instructions);
 
             //Process Instruction
